5af566ee0fad1eb745854d4d90abc841cc07d125
Carla Kechichian <Cocokechichian@gmail.com>
2015-05-21 19:28:55 -0700

    fixing my code

--- index.php
+++ index.php
@@ -11,14 +11,26 @@
 <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 </head>
 
-
-<body>
+<header>
 <link rel="stylesheet" type="text/css" href="main.js">
 <link rel="stylesheet" type="text/css" href="main.css">
 <link rel="stylesheet" type="text/css" href="parallax.css">
-<link rel="stylesheet" type="text/css" href="social.css">
+<!-- <link rel="stylesheet" type="text/css" href="social.css"> --> 
+
 <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 
+
+
+
+<body>
+ <div class="socials">
+  <a href="#"><span>Facebook</span></a>
+  <a href="#"><span>Twitter</span></a>
+  <a href="#"><span>Google+</span></a>
+  <a href="#"><span>Github</span></a>
+  <a href="#"><span>Dribble</span></a>
+  <a href="#"><span>CodePen</span></a>
+</div>
 
 <div id="title" class="slide header">
   <h1>CARLA KESH</h1>
@@ -35,25 +47,24 @@
               </div>
 
 <div id="slide1" class="slide">
-  <div class="title">
+  <div class="parallaxtitle">
     <h1>Slide 1</h1>
-    <p>Lorem ipsum dolor sit amet, in velit iudico mandamus sit, persius dolorum in per, postulant mnesarchum cu nam. Malis movet ornatus id vim, feugait detracto est ea, eam eruditi conceptam in. Ne sit explicari interesset. Labores perpetua cum at. Id viris docendi denique vim.</p>
+    <p> HELLU </p>
   </div>
 </div>
 
 <div id="slide2" class="slide">
-  <div class="title">
+  <div class="parallaxtitle">
     <h1>Slide 2</h1>
-    <p>Lorem ipsum dolor sit amet, in velit iudico mandamus sit, persius dolorum in per, postulant mnesarchum cu nam. Malis movet ornatus id vim, feugait detracto est ea, eam eruditi conceptam in. Ne sit explicari interesset. Labores perpetua cum at. Id viris docendi denique vim.</p>
+    <p> hi</p>
   </div>
-  <img src="http://wallpapers55.com/wp-content/uploads/2013/10/iOS-7-Wallpaper-Parallax-06-1040x600.jpg">
-  <img src="http://wallpapers55.com/wp-content/uploads/2013/10/iOS-7-Wallpaper-Parallax-06-1040x600.jpg"> 
+   
 </div>
 
 <div id="slide3" class="slide">
-  <div class="title">
+  <div class="parallaxtitle">
     <h1>Slide 3</h1>
-    <p>Lorem ipsum dolor sit amet, in velit iudico mandamus sit, persius dolorum in per, postulant mnesarchum cu nam. Malis movet ornatus id vim, feugait detracto est ea, eam eruditi conceptam in. Ne sit explicari interesset. Labores perpetua cum at. Id viris docendi denique vim.</p>
+    <p> HI </p>
   </div>
 </div>
 
@@ -61,49 +72,7 @@
     <h1>The End</h1>
 </div>
 
-<div class="parallax-window" data-parallax="scroll" data-image-src="http://pulpypics.com/wp-content/uploads/2014/08/tumblr-25.jpg"></div>
 
-<div class="bs-example">
-    <div id="myCarousel" class="carousel slide" data-interval="6500" data-ride="carousel">
-    	<!-- Carousel indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>   
-       <!-- Carousel items -->
-        <div class="carousel-inner">
-            <div class="active item carousel-fade">
-                <h2>Slide 1</h2>
-                <div class="carousel-caption">
-                  <h3>First slide label</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-            </div>
-            <div class="item carousel-fade">
-                <h2>Slide 2</h2>
-                <div class="carousel-caption">
-                  <h3>Second slide label</h3>
-                  <p>Aliquam sit amet gravida nibh, facilisis gravida odio.</p>
-                </div>
-            </div>
-            <div class="item carousel-fade">
-                <h2>Slide 3</h2>
-                <div class="carousel-caption">
-                  <h3>Third slide label</h3>
-                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </div>
-            </div>
-        </div>
-        <!-- Carousel nav -->
-        <a class="carousel-control left" href="#myCarousel" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left"></span>
-        </a>
-        <a class="carousel-control right" href="#myCarousel" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right"></span>
-        </a>
-    </div>
-</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="/path/to/parallax.js"></script>
 </body>
