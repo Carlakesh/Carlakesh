11b4083ce4a18a1ed6c5b914957e73bda9988245
Carla Kechichian <Cocokechichian@gmail.com>
2015-05-27 19:44:30 -0700

    dk

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	<title> Carla Kesh | Web Develop </title>
 <link rel="icon" type="iamge/png" href="http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons-256/glossy-black-3d-buttons-icons-alphanumeric/070545-glossy-black-3d-button-icon-alphanumeric-letter-c.png">
 <meta charset="UTF-8">
-<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
-<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css"> 
+ <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css"> 
 <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
 <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
@@ -15,14 +15,15 @@
   <link rel="stylesheet" type="text/css" href="main.js">
 <link rel="stylesheet" type="text/css" href="main.css">
 <link rel="stylesheet" type="text/css" href="parallax.css">
- <link rel="stylesheet" type="text/css" href="social.css"> 
-  <link rel="stylesheet" type="text/css" href="contactform.css"> 
+   <link rel="stylesheet" type="text/css" href="social.css"> 
+ <link rel="stylesheet" type="text/css" href="contactform.css">  
  <link rel="stylesheet" type="text/css" href="nav.css"> 
 <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 </head>
 
 
 <body>
+
  <div id="stickynavbar"> 
 <nav id="stickynavbar">
 <ul class="nav"> 
@@ -34,14 +35,13 @@
 </nav>
  </div> 
 
-
 <div id="title" class="slide header">
   <h1 class="myfont"> CARLA KESH</h1>
   <div id="title2">  PRE-DEVELOPER  ·  STUDENT </div>
 </div>
 
 <div class="socials">
-  <a id="a" href="https://facebook.com/carla.keshishian"><span>Facebook</span></a>
+  <a id="a" href="https://www.facebook.com/carla.keshishian"><span>Facebook</span></a>
   <a id="a" href="https://twitter.com/carlakesh"><span>Twitter</span></a>
   <a id="a" href="https://plus.google.com/u/0/103398563166234981645"><span>Google+</span></a>
   <a id="a" href="https://github.com/carlakesh"><span>Github</span></a>
@@ -59,8 +59,7 @@
     <h1>Slide 2</h1>
     <p> hi</p>
   </div>
-   
-</div>
+   </div>
 
 <div id="slide3" class="slide">
   <div class="parallaxtitle">
@@ -71,8 +70,7 @@
 
 <div id="slide4" class="slide header">
     <h1></h1>
-
-    <div class="transparent_box">
+  <div class="transparent_box">
 <h3> Let's get in touch! </h3>
 
  <form action="MAILTO:Cocokechichian@gmail.com" method="post" enctype="text/plain">
@@ -84,10 +82,8 @@ Message:<br>
 <input type="text" name="comment" value="your message" size="40"><br><br>
 <input id="submit" type="submit" value="Send">
 </form>
-
-</div>
-
-</div>
+  </div>
+    </div>
 
 
 <script src="jquery.js"></script>
--- main.js
+++ main.js
@@ -5,25 +5,15 @@ $(document).ready(function() {
 });
 
 // sticky
-  //$(function () {
+  $(function () {
 
-    //$(window).scroll(function(event) {
-      //if($(this).scrollTop() > 150) {
-      //$("#sitckynavbar").fadeIn();
-      //$("#sitckynavbar").addClass('fixed',1000,'linear');
-    //}
-    //else {
-     // $("#sitckynavbar").removeClass('fixed')
-    //}
-    //});
-  //});
-
-  $(document).ready(function () {
-  var $nav = $('#stickynavbar'),
-      posTop = $nav.position().top;
-  $(window).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > posTop) { $nav.addClass('fixed'); }
-    else { $nav.removeClass('fixed'); }
+    $(window).scroll(function(event) {
+      if($(this).scrollTop() > 150) {
+      $("#sitckynavbar").fadeIn();
+      $("#sitckynavbar").addClass('fixed',1000,'linear');
+    }
+    else {
+      $("#sitckynavbar").removeClass('fixed')
+    }
+    });
   });
-});
